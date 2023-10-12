581e4c75cf509eb261e973c7e1b79e92f875236b
JBisquick <felipebirch@gmail.com>
2023-10-11 18:44:27 -0700

    Add cards that are created from form

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 const libraryContainer = document.querySelector('.library-container');
-const formButton = document.querySelector('#pop-button')
-const popupForm = document.querySelector('.popup-form')
+const formButton = document.querySelector('#pop-button');
+const popupForm = document.querySelector('.popup-form');
+const bookForm = document.querySelector('#book-form');
 
 const myLibrary = [];
 
-formButton.addEventListener('click',  function () {
-  popupForm.classList.add("show");
+bookForm.addEventListener('submit', function (e) {
+  createBookCard(e);
 });
 
-window.addEventListener("click", function (event) {
+formButton.addEventListener('click',  function () {
+  popupForm.classList.add('show');
+});
+
+window.addEventListener('click', function (event) {
   if (event.target == popupForm) {
-    popupForm.classList.remove("show");
+    popupForm.classList.remove('show');
   }
 });
 
-function Book(name) {
-  this.name = name;
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary(book) {
@@ -27,8 +35,29 @@ function showBookCards(library) {
     const bookCard = document.createElement('div');
     bookCard.classList.add('book-card');
     libraryContainer.appendChild(bookCard);
+
     const bookTitle = document.createElement('div');
     bookCard.appendChild(bookTitle);
-    bookTitle.textContent = book.name;
+    bookTitle.textContent = book.title;
+    const bookAuthor = document.createElement('div');
+    bookCard.appendChild(bookAuthor);
+    bookAuthor.textContent = book.author;
+    const bookPages = document.createElement('div');
+    bookCard.appendChild(bookPages);
+    bookPages.textContent = book.pages;
   }
+}
+
+function createBookCard(event) {
+  event.preventDefault();
+  popupForm.classList.remove('show')
+
+  let title = document.getElementById('title').value;
+  let author =  document.getElementById('author').value;
+  let pages =  document.getElementById('pages').value;
+  let read = document.getElementById('read').value;
+
+  const newBook = new Book(title, author, pages, read); 
+  addBookToLibrary(newBook);
+  showBookCards(myLibrary);
 }
